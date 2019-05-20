ca6ba92c37adf602201125c8d5f966d43be7107e
Ibrahim Olayinka <ibrahimolayinkaa@gmail.com>
2019-05-20 16:15:48 +0100

    first commit

---
+++ config/configuration.js
@@ -0,0 +1,5 @@
+module.exports = {
+
+    mongoDBUrl : "mongodb://localhost:27017/blogCMS_DB",
+    PORT: process.env.PORT || 3300
+}
---
+++ index.js
@@ -0,0 +1,56 @@
+// importing different modules
+const express = require('express');
+const mongoose = require('mongoose');
+const path = require('path');
+const hbs = require('express-handlebars');
+const {mongoDBUrl,PORT} = require('./config/configuration');
+const app = new express();
+
+
+//configure mongoose to connect MongoDB
+
+mongoose.connect(mongoDBUrl,{useNewUrlParser:true})
+        .then(response=>{
+            console.log("MongoDB connected successfully ");
+        }).catch(err=>{
+            console.log('connection to MongoDB failed '+err);
+        });
+
+
+//configure express
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+//public asset folder setup
+app.use(express.static(path.join(__dirname,'public')));
+
+
+// set up view engine to use handlebars
+/*
+app.set('views',path.join(__dirname,'/views/'));
+app.engine('hbs', exphbs({extname:'hbs', defaultLayout: 'default',layoutsDir:__dirname+'/views/layouts/'}));
+app.set('view engine','hbs');
+
+*/
+//set up second method with the default extension name
+app.engine('handlebars', hbs({defaultLayout: 'default'}));
+app.set('view engine','handlebars');
+
+
+//
+
+
+//routes
+app.get('/',(req,res)=>{
+
+    res.render('default/index');
+
+});
+
+
+
+
+// server started
+app.listen(PORT,()=>{
+    console.log(`server is running on port ${PORT}`);
+});
